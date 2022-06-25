57f5c51033559aeb94d207cf82dc3338a6280878
Yida Chen <yc015@bucknell.edu>
2022-06-24 23:21:16 -0400

    final check for alpha pre-release of v1.3.13

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path_to_md, encoding='utf-8') as f:
 f.close()
 
 setup(name='kalmus',
-      version='1.3.13a3',
+      version='1.3.13a4',
       description='kalmus film color analysis tool',
       keywords='film, color analysis, data visualization',
       long_description=readme,
